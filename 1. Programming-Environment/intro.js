4117ace02bc502aea81e5b1248c2d01bb46caed6
fehfialho <fernandaklechowicz@gmail.com>
2025-09-02 11:37:39 -0300

    Rename + Date

--- 1. Programming-Environment/intro.js
+++ 1. Programming-Environment/intro.js
@@ -1,3 +1,5 @@
+// Lesson 1 - Programming Environments for JavaScript (09/02/2025)
+
 // let rlsync = require('readline-sync');
 // let name = rlsync.question("What's your name?\n")
 // console.log(`Welcome, ${name}!`);
