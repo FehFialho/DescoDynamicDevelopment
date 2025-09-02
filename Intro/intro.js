f3c8f7fc3df690c2259b67533a9821f640312d1c
fehfialho <fernandaklechowicz@gmail.com>
2025-09-02 11:33:10 -0300

    Readline-Sync and Live Server

---
+++ Intro/intro.js
@@ -0,0 +1,8 @@
+// let rlsync = require('readline-sync');
+// let name = rlsync.question("What's your name?\n")
+// console.log(`Welcome, ${name}!`);
+
+let firstname = prompt("What's your first name?");
+let surname = prompt("What's your surname?");
+
+alert(`Your complete name is ${firstname} ${surname}!`);
